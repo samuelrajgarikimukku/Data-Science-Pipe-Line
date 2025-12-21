f7f6e436ecd2b8ade52c5f155d9c139bd3568f78
Samuel Raj G <samuelgariki11@gmail.com>
2025-12-22 00:03:49 +0530

    Model trainer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.datascienceproject.exception import CustomException
 from src.datascienceproject.components.data_ingestion import DataIngestion
 from src.datascienceproject.components.data_ingestion import DataIngestionConfig
 from src.datascienceproject.components.data_transformation import DataTransformationConfig, DataTransformation
+from src.datascienceproject.components.model_tranier import ModelTrainer, ModelTrainerConfig
 import sys
 
 if __name__ == "__main__":
@@ -14,10 +15,14 @@ if __name__ == "__main__":
         train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
 
-
         # data_transformation_config=DataIngestionConfig()
         data_transformation = DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        ## Model Training
+        model_trainer = ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 
     except Exception as e:
         logging.info('Custom Exception')
--- src/datascienceproject/utils.py
+++ src/datascienceproject/utils.py
@@ -5,6 +5,8 @@ from src.datascienceproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import psycopg2 
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
 import pickle
 import numpy as np
@@ -24,16 +26,12 @@ def read_sql_data():
             user=user,
             password=password,
             dbname=db
-            
         )
+
         logging.info("Connection Established",mydb)
         df=pd.read_sql_query('Select * from students',mydb)
         print(df.head())
-
-
         return df 
-
-
 
     except Exception as ex:
         raise CustomException(ex,sys)
@@ -42,10 +40,34 @@ def read_sql_data():
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path,exist_ok=True)
-
         with open(file_path,"wb") as file_obj:
             pickle.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
     except Exception as e:
         raise CustomException(e, sys)
